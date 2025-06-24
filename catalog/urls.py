36c252cf403005b240502f56d5c0a80f09d55240
Pavel <shegol164@mail.ru>
2025-06-24 20:23:35 +0400

    Добавлениение catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,9 +3,14 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import ProductDetailView, ProductDetailView, ContactsView
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('', include('catalog.urls')),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, TemplateView
+from django.shortcuts import render
 from .models import Product
 
+# Класс для главной страницы
+class HomeView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    context_object_name = 'products'
+
+# Класс для страницы товара
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+
+# Класс для страницы контактов
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
+
+# Альтернативный вариант FBV (если нужно)
+'''
 def home(request):
-    return render(request, 'catalog/home.html')
+    products = Product.objects.all()
+    return render(request, 'catalog/home.html', {'products': products})
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
-
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, 'catalog/product_detail.html', {'product': product})
-
-def home(request):
-    products = Product.objects.all()[:10]
-    return render(request, 'catalog/home.html', {'products': products})
+'''

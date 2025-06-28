825cb648b256d6bc19d2e1aa0b1478a4a15aa5d2
Pavel <shegol164@mail.ru>
2025-06-28 10:59:24 +0400

    Домашнее задание по теме 26.1

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,35 @@
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import (ListView, DetailView,
+                                 CreateView, UpdateView,
+                                 DeleteView, TemplateView)
+from django.urls import reverse_lazy
 from .models import Product
+from .forms import ProductForm
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'catalog/home.html'  # Используем существующий шаблон
+    template_name = 'catalog/product_list.html'
     context_object_name = 'products'
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'catalog/product_detail.html'
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:product_list')
 
 class ContactsView(TemplateView):
     template_name = 'catalog/contacts.html'
